788b448d46961b21051266500a8d57d6d3435df1
Wietse Braam <wietse.braam@ing.com>
2019-09-05 14:40:02 +0200

    Fixed change to integers

--- tests/test_course2_solution.py
+++ tests/test_course2_solution.py
@@ -33,47 +33,47 @@ def test_sum_with_invalid_input_should_return_bad_request(test_client):
 def test_compute_add(test_client):
     """test for the /compute/add endpoint"""
 
-    data = {'value1': '1', 'value2': '2'}
+    data = {'value1': 1, 'value2': 2}
     url = '/compute/add'
     response = test_client.post(url, data=data)
     assert response.status_code == 200
-    assert response.data == b'3\n'
+    assert response.data == 3'
 
 
 def test_compute_subtract(test_client):
     """test for the /compute/subtract endpoint"""
 
-    data = {'value1': '5', 'value2': '3'}
+    data = {'value1': 5, 'value2': 3}
     url = '/compute/subtract'
     response = test_client.post(url, data=data)
     assert response.status_code == 200
-    assert response.data == b'2\n'
+    assert response.data == 2'
 
 
 def test_compute_multiply(test_client):
     """test for the /compute/multiply endpoint"""
 
-    data = {'value1': '5', 'value2': '3'}
+    data = {'value1': 5, 'value2': 3}
     url = '/compute/multiply'
     response = test_client.post(url, data=data)
     assert response.status_code == 200
-    assert response.data == b'15\n'
+    assert response.data == 15
 
 
 def test_compute_divide(test_client):
     """test for the /compute/divide endpoint"""
 
-    data = {'value1': '6', 'value2': '3'}
+    data = {'value1': 6, 'value2': 3}
     url = '/compute/divide'
     response = test_client.post(url, data=data)
     assert response.status_code == 200
-    assert response.data == b'2.0\n'
+    assert response.data == 2.0
 
 
 def test_compute_compute_bad_request(test_client):
     """test for the /compute endpoint with invalid param"""
 
-    data = {'value': '6', 'value2': '3'}
+    data = {'value': 6, 'value2': 3}
     url = '/compute/divide'
     response = test_client.post(url, data=data)
     assert response.status_code == 400
@@ -82,7 +82,7 @@ def test_compute_compute_bad_request(test_client):
 def test_compute_compute_bad_request_divide_by_zero(test_client):
     """test for the /compute endpoint corner case"""
 
-    data = {'value': '6', 'value2': '0'}
+    data = {'value': 6, 'value2': 0}
     url = '/compute/divide'
     response = test_client.post(url, data=data)
     assert response.status_code == 400
